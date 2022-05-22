f917a97aea6c3bba20290e4ff5dcb895a527cb64
Kaden Hansen <kadendeanhansen@gmail.com>
2022-05-21 21:12:07 -0400

    add max number function

---
+++ functions.py
@@ -0,0 +1,13 @@
+print("max_num:")
+def max_num(num1, num2, num3):
+    list = [num1, num2, num3]
+    return max(list)
+print(max_num(4, 7, 10))
+
+print("mult_list:")
+
+print("rev_string:")
+
+print("num_within:")
+
+print("pascal:")
