d11496bf99f979d0b050c85b38fc09d3abe2e039
Kaden Hansen <kadendeanhansen@gmail.com>
2022-05-21 21:46:30 -0400

    add multiply list function

--- functions.py
+++ functions.py
@@ -1,10 +1,19 @@
+# max_num function
 print("max_num:")
 def max_num(num1, num2, num3):
     list = [num1, num2, num3]
     return max(list)
 print(max_num(4, 7, 10))
 
+# mult_list function
 print("mult_list:")
+def mult_list(numbers):
+    total = 1
+    for num in numbers:
+        total *= num
+    return total
+numbers = [3, 10, 5]
+print(mult_list(numbers))
 
 print("rev_string:")
 
